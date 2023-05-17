8e4984cf4598e1732befc8058e5afe2b09fa096f
keiichi <abdelkareem.siddiq@gmail.com>
2023-05-17 00:04:48 -0700

    Remove intermediate folders

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kei/projects/TimeThis
-# Build directory: /Users/kei/projects/TimeThis/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("include")
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kei/projects/TimeThis/tests
-# Build directory: /Users/kei/projects/TimeThis/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[TimeThis_gtests]=] "tests")
-set_tests_properties([=[TimeThis_gtests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/kei/projects/TimeThis/tests/CMakeLists.txt;23;add_test;/Users/kei/projects/TimeThis/tests/CMakeLists.txt;0;")
-subdirs("../_deps/googletest-build")
